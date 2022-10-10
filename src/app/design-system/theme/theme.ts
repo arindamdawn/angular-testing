bd79815a75d1d53b5934e06155fbd9f47ca02850
Arindam <arindamdawn3@gmail.com>
2022-10-10 10:30:51 +0530

    initial commit

---
+++ src/app/design-system/theme/theme.ts
@@ -0,0 +1,16 @@
+export const THEMES = {
+  light: {
+    primaryColor: 'hsl(185, 57%, 35%)',
+    secondaryColor: 'hsl(0, 0%, 22%)',
+    textOnPrimary: 'hsl(0, 0%, 100%)',
+    textOnSecondary: 'hsl(0, 0%, 90%)',
+    background: 'hsl(0, 0%, 100%)',
+  },
+  dark: {
+    primaryColor: 'hsl(168deg 100% 29%)',
+    secondaryColor: 'hsl(161deg 94% 13%)',
+    textOnPrimary: 'hsl(0, 0%, 100%)',
+    textOnSecondary: 'hsl(0, 0%, 100%)',
+    background: 'hsl(0, 0%, 10%)',
+  },
+};
